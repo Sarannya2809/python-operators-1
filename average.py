702ddcc610b53cf5380609d5b52ba0a658f18809
Sarannya2809 <Sarannyaagarwal28@gmail.com>
2025-07-31 15:54:58 +0400

    Add files via upload

---
+++ average.py
@@ -0,0 +1,10 @@
+
+tree1=28
+tree2=23
+tree3=7
+tree4=87
+tree5=56
+sum= tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 trees is: ", sum )
+average=sum/5
+print("the average of the tree is:" , average)
